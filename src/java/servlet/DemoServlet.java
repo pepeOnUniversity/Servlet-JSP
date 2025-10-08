7d74e8bd7429f645400b7cb914f91ec9dcead4bc
pepeOnUniversity <contact.me.dothehung@gmail.com>
2025-10-08 18:36:47 +0700

    create DemoServlet

--- src/java/servlet/DemoServlet.java
+++ src/java/servlet/DemoServlet.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
 
+package servlet;
+
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,49 +13,32 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  *
  * @author ADMIN
  */
-@WebServlet(urlPatterns = {"/DemoServlet"})
+@WebServlet(name="DemoServlet", urlPatterns={"/DemoServlet"})
 public class DemoServlet extends HttpServlet {
-
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
+   
+    /** 
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-
-        //create List students
-        List<Student> listStudents = new ArrayList<>();
-        //create 1 list objects
-        Student student1 = new Student(1, "Do The Hung");
-        Student student2 = new Student(2, "Do Anh Thu");
-        Student student3 = new Student(3, "Nguyen Van A");
-        //add list student
-        listStudents.add(student1);
-        listStudents.add(student2);
-        listStudents.add(student3);
-        //set attribute
-        request.setAttribute("listStudents", listStudents);
+        
         RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
         rd.forward(request, response);
-    }
+    } 
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
+    /** 
      * Handles the HTTP <code>GET</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -61,13 +46,12 @@ public class DemoServlet extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
-    }
+    } 
 
-    /**
+    /** 
      * Handles the HTTP <code>POST</code> method.
-     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -75,13 +59,12 @@ public class DemoServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+    throws ServletException, IOException {
         processRequest(request, response);
     }
 
-    /**
+    /** 
      * Returns a short description of the servlet.
-     *
      * @return a String containing servlet description
      */
     @Override
